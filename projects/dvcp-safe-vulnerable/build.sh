b18be6efdde6d5901a43cac206deb58f3ac022bf
Ubuntu <azureuser@Anjana-Linux-VM2.avmyxydaoftehh1k4xefuaksuf.xx.internal.cloudapp.net>
2025-11-24 20:30:53 +0000

    Link fuzzer against actual dvcp.c to test safe vs vulnerable branches
    - Created dvcp_fuzz_linked.c that calls ProcessImage from dvcp.c
    - Updated build.sh to compile and link dvcp.c
    - This allows Buttercup to properly test diff between safe and vulnerable code

--- projects/dvcp-safe-vulnerable/build.sh
+++ projects/dvcp-safe-vulnerable/build.sh
@@ -1,30 +1,15 @@
 #!/bin/bash -eu
-# Copyright 2024 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-################################################################################
 
-# Build the fuzzer
+# Build dvcp.c as an object file
+$CC $CFLAGS -c $SRC/dvcp/dvcp.c -o $SRC/dvcp.o
+
+# Build the fuzzer and link with dvcp.o
 $CC $CFLAGS $LIB_FUZZING_ENGINE \
     -I$SRC/dvcp \
-    $SRC/dvcp_fuzz.c \
+    $SRC/dvcp_fuzz_linked.c \
+    $SRC/dvcp.o \
     -o $OUT/dvcp_fuzz
 
-# Create initial corpus
+# Create seed corpus
 mkdir -p $OUT/dvcp_fuzz_seed_corpus
-
-# Create some seed inputs
 echo -ne "IMG\x00\x01\x00\x00\x00\x01\x00\x00\x00AAAAAAAAAA" > $OUT/dvcp_fuzz_seed_corpus/seed1
-echo -ne "IMG\x00\x10\x00\x00\x00\x10\x00\x00\x00BBBBBBBBBB" > $OUT/dvcp_fuzz_seed_corpus/seed2
-echo -ne "IMG\x00\xff\xff\xff\x7f\x10\x00\x00\x00CCCCCCCCCC" > $OUT/dvcp_fuzz_seed_corpus/seed3
---
+++ projects/dvcp-safe-vulnerable/dvcp_fuzz_linked.c
@@ -0,0 +1,33 @@
+/*
+ * Fuzzer harness that links against actual dvcp.c
+ */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+// Declare the function from dvcp.c
+int ProcessImage(char* filename);
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    if (size < 18) {
+        return 0;
+    }
+
+    // Write fuzzer input to a temporary file
+    char tmpfile[] = "/tmp/fuzz_input_XXXXXX";
+    int fd = mkstemp(tmpfile);
+    if (fd == -1) return 0;
+    
+    write(fd, data, size);
+    close(fd);
+    
+    // Call the actual ProcessImage function from dvcp.c
+    ProcessImage(tmpfile);
+    
+    // Clean up
+    unlink(tmpfile);
+    
+    return 0;
+}
