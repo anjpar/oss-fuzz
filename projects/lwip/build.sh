c122f7f40426bd5019c2b3be9cee97e8b6855aa6
Ubuntu <azureuser@Anjana-Linux-VM2.avmyxydaoftehh1k4xefuaksuf.xx.internal.cloudapp.net>
2025-12-08 20:02:39 +0000

    Fix lwIP build.sh - compile libraries only, not AFL fuzzers

--- projects/lwip/build.sh
+++ projects/lwip/build.sh
@@ -2,23 +2,29 @@
 
 cd $SRC/lwip/test/fuzz
 
-# Build lwIP libraries with fuzzing instrumentation
-CC=clang CFLAGS="$CFLAGS -O1 -fsanitize=fuzzer-no-link,address,undefined -Wno-error" make clean all
+# Build lwIP libraries with fuzzing instrumentation (but skip AFL fuzzers)
+CC=clang CFLAGS="$CFLAGS -O1 -fsanitize=fuzzer-no-link,address,undefined -Wno-error" make clean
+CC=clang CFLAGS="$CFLAGS -O1 -fsanitize=fuzzer-no-link,address,undefined -Wno-error" make liblwipapps.a liblwipcommon.a
+
+# Compile fuzz_common.o separately with correct flags
+$CC $CFLAGS -O1 -fsanitize=fuzzer-no-link,address,undefined -Wno-error \
+    -I../../src/include -I../../contrib/ports/unix/port/include -I. \
+    -c fuzz_common.c -o fuzz_common.o
 
 # Build fuzzer 1: SINGLE mode
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE \
+$CXX $CXXFLAGS -O1 $LIB_FUZZING_ENGINE \
     -I../../src/include -I../../contrib/ports/unix/port/include -I. \
     fuzz_lwip_libfuzzer.c fuzz_common.o lwip_fuzzer_stubs.c \
     liblwipapps.a liblwipcommon.a -lm -o $OUT/fuzz_lwip_single
 
 # Build fuzzer 2: MULTIPACKET mode
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE \
+$CXX $CXXFLAGS -O1 $LIB_FUZZING_ENGINE \
     -I../../src/include -I../../contrib/ports/unix/port/include -I. \
     fuzz_lwip_libfuzzer2.c fuzz_common.o lwip_fuzzer_stubs.c \
     liblwipapps.a liblwipcommon.a -lm -o $OUT/fuzz_lwip_multipacket
 
 # Build fuzzer 3: MULTIPACKET_TIME with all apps
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE \
+$CXX $CXXFLAGS -O1 $LIB_FUZZING_ENGINE \
     -I../../src/include -I../../contrib/ports/unix/port/include -I. \
     fuzz_lwip_libfuzzer3.c fuzz_common.o lwip_fuzzer_stubs.c \
     liblwipapps.a liblwipcommon.a -lm -o $OUT/fuzz_lwip_full
